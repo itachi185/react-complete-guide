1f1d001025686cd318bd37fe7aa0fb4a186dbcb0
itachi185 <you@example.com>
2021-10-27 22:09:45 +0700

    added code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { id: Math.random().toString(), name: uName, age: uAge },
+      ];
+    });
+  };
+
+  console.log("userList in App.js: ", usersList);
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
