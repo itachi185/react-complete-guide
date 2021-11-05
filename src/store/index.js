870296abc0fbddeb8ddc36e39df3446831eddde9
itachi185 <you@example.com>
2021-11-05 15:33:44 +0700

    added code

--- src/store/index.js
+++ src/store/index.js
@@ -1,50 +1,13 @@
-// import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
-import counterReducer from "./counter";
-import authReducer from "./auth";
+import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    auth: authReducer,
+    ui: uiSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
 
 export default store;
-
-// const counterReducer = (state = initialState, action) => {
-//   if (action.type === "increment") {
-//     return {
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "increase") {
-//     return {
-//       counter: state.counter + action.amount,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "decrement") {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "toggle") {
-//     return {
-//       showCounter: !state.showCounter,
-//       counter: state.counter,
-//     };
-//   }
-
-//   return state;
-// };
-
-// const store = createStore(counterReducer);
-
-// export default store;
